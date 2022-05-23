d20d046f624ee965652b8aaed5bfa706dd078c36
Windisch Matthias <matthias.windisch@edu.fh-joanneum.at>
2022-05-23 13:14:42 +0200

    first commit

---
+++ src/main/kotlin/at/fhjoanneum/myusi/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package at.fhjoanneum.myusi
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(MyUsiApplication::class.java)
+    }
+
+}
---
+++ src/test/kotlin/at/fhjoanneum/myusi/MyUsiApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.fhjoanneum.myusi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyUsiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
