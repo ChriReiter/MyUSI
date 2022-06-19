9ed3b3484c126d64d51614c244ca456d278f116a
Christian Reiter <christian.reiter3@edu.fh-joanneum.at>
2022-06-19 09:53:01 +0200

    added compareTo(), equals() and hashCode() to entities

--- src/main/kotlin/at/fhjoanneum/myusi/Entity/Course.kt
+++ src/main/kotlin/at/fhjoanneum/myusi/Entity/Course.kt
@@ -2,6 +2,7 @@ package at.fhjoanneum.myusi.Entity
 
 import jdk.jfr.Timestamp
 import org.springframework.format.annotation.DateTimeFormat
+import java.io.Serializable
 import java.sql.Time
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -39,6 +40,21 @@ class Course (
 //    var file: File? = null
     //@OneToMany
     //var dates: List<CourseDate>? =  null
-){
+) : Comparable<Course>, Serializable {
 
+    override fun compareTo(other: Course): Int {
+        return compareValues(id, other.id)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Course
+        if (id != other.id) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
--- src/main/kotlin/at/fhjoanneum/myusi/Entity/CourseDate.kt
+++ src/main/kotlin/at/fhjoanneum/myusi/Entity/CourseDate.kt
@@ -1,6 +1,7 @@
 package at.fhjoanneum.myusi.Entity
 
 import org.springframework.format.annotation.DateTimeFormat
+import java.io.Serializable
 import java.sql.Time
 import java.time.LocalDate
 import javax.persistence.Entity
@@ -16,6 +17,20 @@ class CourseDate(
     var date: LocalDate? = null,
     var timeStart: Time? = null,
     var timeEnd: Time? = null,
-    //var Location
-    ) {
+) : Comparable<CourseDate>, Serializable {
+    override fun compareTo(other: CourseDate): Int {
+        return compareValues(id, other.id)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as CourseDate
+        if (id != other.id) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
